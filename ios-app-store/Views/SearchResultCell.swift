3fec1902baa1a8db07049565c5cf0b61f1e4e30c
Paul Defilippi <allinfo.pd@gmail.com>
2019-09-16 15:00:36 -0400

    added a few extentions and helper files

--- ios-app-store/Views/SearchResultCell.swift
+++ ios-app-store/Views/SearchResultCell.swift
@@ -13,7 +13,7 @@ class SearchResultCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .yellow
+        backgroundColor = .white
 //        getButton.layer.cornerRadius = getButton.frame.height / 2
 //        getButton.clipsToBounds = true
         
@@ -22,25 +22,41 @@ class SearchResultCell: UICollectionViewCell {
             ])
         labelsStackView.axis = .vertical
         
-        let stackView = UIStackView(arrangedSubviews: [
-            imageView, labelsStackView, getButton
+        let infoTopStackView = UIStackView(arrangedSubviews: [
+            appIconImageView, labelsStackView, getButton
             ])
-        stackView.spacing = 12
-        stackView.alignment = .center
+        infoTopStackView.spacing = 12
+        infoTopStackView.alignment = .center
         
-        addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        let screenShotStackView = UIStackView(arrangedSubviews: [
+            screenshot1ImageView, screenshot2ImageView, screenshot3ImageView
+            ])
+        screenShotStackView.distribution = .fillEqually
+        screenShotStackView.spacing = 12
+        
+        let overallStackView = UIStackView(arrangedSubviews: [
+            infoTopStackView, screenShotStackView
+            
+            ])
+        overallStackView.axis = .vertical
+        overallStackView.spacing = 16
+        
+        
+        
+        addSubview(overallStackView)
+        overallStackView.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+//        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+//        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+//        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    let imageView: UIImageView = {
+    let appIconImageView: UIImageView = {
         let iv = UIImageView()
         iv.backgroundColor = .red
         iv.layer.cornerRadius = 12
@@ -73,10 +89,23 @@ class SearchResultCell: UICollectionViewCell {
         button.setTitleColor(.blue, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 14)
         button.backgroundColor = UIColor(white: 0.95, alpha: 1)
-        button.layer.cornerRadius = 16
-//        button.clipsToBounds = true
+        //        button.clipsToBounds = true
         button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        button.layer.cornerRadius = 16
+
         return button
     }()
+    
+    lazy var screenshot1ImageView = self.createScreenshotImageView()
+    lazy var screenshot2ImageView = self.createScreenshotImageView()
+    lazy var screenshot3ImageView = self.createScreenshotImageView()
+    
+    
+    func createScreenshotImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        return imageView
+    }
     
 }

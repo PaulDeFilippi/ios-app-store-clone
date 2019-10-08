b3398fbce4bfd1a9dcbae6ce7b78567619ca3957
Paul Defilippi <allinfo.pd@gmail.com>
2019-10-08 15:02:56 -0400

    implemented app detail cell and some stack views

--- ios-app-store/Controller/Apps/Details/AppDetailController.swift
+++ ios-app-store/Controller/Apps/Details/AppDetailController.swift
@@ -8,9 +8,39 @@
 
 import UIKit
 
-class AppDetailController: BaseListController {
+class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    var appId: String! {
+        didSet {
+            print("Here is my appId: ", appId)
+            let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
+            
+            APIService.shared.fetchGenericJSONData(urlString: urlString) { (result: SearchResult?, err) in
+                print(result?.results.first?.releaseNotes)
+            }
+        }
+    }
+    
+    let detailCellID = "detailCellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: detailCellID)
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellID, for: indexPath) as! AppDetailCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
     }
 }
---
+++ ios-app-store/Views/Apps/Details/AppDetailCell.swift
@@ -0,0 +1,70 @@
+//
+//  AppDetailCell.swift
+//  ios-app-store
+//
+//  Created by Paul Defilippi on 10/7/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //backgroundColor = .red
+        
+        appIconImageView.backgroundColor = .red
+        appIconImageView.constrainWidth(constant: 140)
+        appIconImageView.constrainHeight(constant: 140)
+        
+        priceButton.backgroundColor = #colorLiteral(red: 0.002984073712, green: 0.4764925241, blue: 0.9985365272, alpha: 1)
+        priceButton.constrainHeight(constant: 32)
+        priceButton.layer.cornerRadius = 32 / 2
+        priceButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        priceButton.setTitleColor(.white, for: .normal)
+        priceButton.constrainWidth(constant: 80)
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            
+            UIStackView(arrangedSubviews: [
+                appIconImageView,
+                VerticalStackView(arrangedSubviews: [
+                    nameLabel,
+                    UIStackView(arrangedSubviews: [priceButton, UIView()]),
+                    UIView()
+                    ], spacing: 12)
+                ], customSpacing: 20),
+            
+            
+            whatsNewLabel,
+            releaseNotesLabel
+            
+            ], spacing: 16)
+        
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 20, left: 20, bottom: 20, right: 20))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let appIconImageView = UIImageView(cornerRadius: 16)
+    
+    let nameLabel = UILabel(text: "App Name", font: .boldSystemFont(ofSize: 24), numberOfLines: 2)
+    
+    let priceButton = UIButton(title: "$4.99")
+    
+    let whatsNewLabel = UILabel(text: "What's New", font: .boldSystemFont(ofSize: 20))
+    
+    let releaseNotesLabel = UILabel(text: "Release Notes", font: .systemFont(ofSize: 16), numberOfLines: 0)
+}
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], customSpacing: CGFloat = 0) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.spacing = customSpacing
+    }
+}

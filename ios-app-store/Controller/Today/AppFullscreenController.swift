55231e3020b71158b1d872f7d2e69cc990214543
Paul Defilippi <allinfo.pd@gmail.com>
2019-11-08 17:17:46 -0500

    adjusted today cell background color and animations

--- ios-app-store/Controller/Today/AppFullscreenController.swift
+++ ios-app-store/Controller/Today/AppFullscreenController.swift
@@ -21,6 +21,10 @@ class AppFullscreenController: UITableViewController {
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
+        tableView.contentInsetAdjustmentBehavior = .never
+        
+        let height = UIApplication.shared.statusBarFrame.height
+        tableView.contentInset = .init(top: 0, left: 0, bottom: height, right: 0)
 
     }
     
@@ -43,6 +47,7 @@ class AppFullscreenController: UITableViewController {
             let headerCell = AppFullscreenHeaderCell()
             headerCell.closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
             headerCell.todayCell.todayItem = todayItem
+            headerCell.todayCell.layer.cornerRadius = 0
             return headerCell
         }
         

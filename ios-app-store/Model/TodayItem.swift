834832983ae42e68d07afde12ddad9469d800cc6
Paul Defilippi <allinfo.pd@gmail.com>
2019-11-08 11:07:09 -0500

    framed out dynamic today cells

---
+++ ios-app-store/Model/TodayItem.swift
@@ -0,0 +1,19 @@
+//
+//  TodayItem.swift
+//  ios-app-store
+//
+//  Created by Paul Defilippi on 11/6/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+struct TodayItem {
+    
+    let category: String
+    let title: String
+    let image: UIImage
+    let description: String
+    let backgroundColor: UIColor
+    
+}
--- ios-app-store/Views/Today/TodayCell.swift
+++ ios-app-store/Views/Today/TodayCell.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class TodayCell: UICollectionViewCell {
     
+    var todayItem: TodayItem! {
+        didSet {
+            categoryLabel.text = todayItem.category
+            titleLabel.text = todayItem.title
+            imageView.image = todayItem.image
+            descriptionLabel.text = todayItem.description
+        }
+    }
+    
+    // MARK:- Initialization
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .white
+        clipsToBounds = true
         layer.cornerRadius = 16
         
-        addSubview(imageView)
+        //addSubview(imageView)
         imageView.contentMode = .scaleAspectFill
-        imageView.centerInSuperview(size: .init(width: 230, height: 230))
+        //imageView.centerInSuperview(size: .init(width: 230, height: 230))
+        
+        let imageContainerView = UIView()
+        imageContainerView.addSubview(imageView)
+        imageView.centerInSuperview(size: .init(width: 240, height: 240))
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            
+            categoryLabel, titleLabel, imageContainerView, descriptionLabel
+            ], spacing: 8)
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 24, left: 24, bottom: 24, right: 24))
         
     }
     
@@ -26,5 +49,13 @@ class TodayCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK:- Views
+    
+    let categoryLabel = UILabel(text: "LIFE HACK", font: .boldSystemFont(ofSize: 20))
+    
+    let titleLabel = UILabel(text: "Utilizing your time", font: .boldSystemFont(ofSize: 28))
+    
     let imageView = UIImageView(image: #imageLiteral(resourceName: "garden"))
+    
+    let descriptionLabel = UILabel(text: "All the tools and apps you need to intelligently organize your life the right way.", font: .systemFont(ofSize: 16), numberOfLines: 3)
 }
